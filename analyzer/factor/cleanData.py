7ad3b183f2f7a744da8a5ad326b033a54e1a0056
iLampard <siqiao_xue@163.com>
2016-12-25 00:31:42 +0800

    Remove type check

--- analyzer/factor/cleanData.py
+++ analyzer/factor/cleanData.py
@@ -103,14 +103,10 @@ def getMultiIndexData(multiIdxData, firstIdxName, firstIdxVal, secIdxName=None, 
     Returns: pd.Series, selected value with multi-index = [firstIdxName, secIdxName]
 
     """
-    pyFinAssert(isinstance(firstIdxVal, basestring) or isinstance(firstIdxVal, list), TypeError,
-                "input first index value type must be str or list")
     if isinstance(firstIdxVal, basestring):
         firstIdxVal = [firstIdxVal]
     data = multiIdxData.loc[multiIdxData.index.get_level_values(firstIdxName).isin(firstIdxVal)]
     if secIdxName is not None:
-        pyFinAssert(isinstance(secIdxVal, basestring) or isinstance(secIdxVal, list), TypeError,
-                    "input second index value type must be str or list")
         if isinstance(secIdxVal, basestring):
             secIdxVal = [secIdxVal]
         data = data.loc[data.index.get_level_values(secIdxName).isin(secIdxVal)]

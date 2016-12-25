39d3fd5dea48810db97764b6fc5de5ab101494c2
iLampard <siqiao_xue@163.com>
2016-12-25 21:16:23 +0800

    Add test for getReportDate

--- tests/analyzer/factor/testCleanData.py
+++ tests/analyzer/factor/testCleanData.py
@@ -2,9 +2,17 @@
 import unittest
 import pandas as pd
 from analyzer.factor.cleanData import getMultiIndexData
-
+from analyzer.factor.cleanData import getReportDate
 
 class TestCleanData(unittest.TestCase):
+    def testGetReportDate(self):
+        actDate = ['2015-01-01', '2015-03-30', '2015-06-20', '2016-09-01', '2016-12-05']
+        calculated = [getReportDate(date) for date in actDate]
+        expected = ['2014-09-30', '2014-09-30', '2015-03-31', '2016-06-30', '2016-09-30']
+        for i in range(len(expected)):
+            self.assertEqual(calculated[i], expected[i], "Expected report date of {0} is not equal to expected {1}"
+                             .format(actDate[i], expected[i]))
+
     def testGetMultiIndexData(self):
         index = pd.MultiIndex.from_arrays(
             [['2015-01-01', '2015-01-02', '2015-01-02', '2015-02-04', '2015-02-04'], ['A', 'B', 'C', 'A', 'C']],

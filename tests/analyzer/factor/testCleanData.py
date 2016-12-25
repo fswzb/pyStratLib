122a0e11eb81c43450d548a0bc6cec0f47bf4df6
iLampard <siqiao_xue@163.com>
2016-12-25 20:51:37 +0800

    Forgot to checkin in last commit

--- tests/analyzer/factor/testCleanData.py
+++ tests/analyzer/factor/testCleanData.py
@@ -1,8 +1,6 @@
 # -*- coding: utf-8 -*-
 import unittest
-
 import pandas as pd
-
 from analyzer.factor.cleanData import getMultiIndexData
 
 
@@ -14,36 +12,27 @@ class TestCleanData(unittest.TestCase):
         multi = pd.Series([1.0, 2.0, 3.0, 4.0, 3.0], index=index)
 
         calculated = getMultiIndexData(multi, 'date', '2015-01-02')
-        expected = pd.Series([2.0, 3.0], index=pd.MultiIndex(
-            levels=[[u'2015-01-01', u'2015-01-02', u'2015-02-04'], [u'A', u'B', u'C']],
-            labels=[[1, 1], [1, 2]],
-            names=[u'date', u'category']))
+        expected = pd.Series([2.0, 3.0], index=pd.MultiIndex.from_arrays(
+            [['2015-01-02', '2015-01-02'], ['B','C']],
+            names=['date', 'category']))
         pd.util.testing.assert_series_equal(calculated, expected)
 
         calculated = getMultiIndexData(multi, 'date', ['2015-01-02'])
-        expected = pd.Series([2.0, 3.0], index=pd.MultiIndex(
-            levels=[[u'2015-01-01', u'2015-01-02', u'2015-02-04'], [u'A', u'B', u'C']],
-            labels=[[1, 1], [1, 2]],
-            names=[u'date', u'category']))
         pd.util.testing.assert_series_equal(calculated, expected)
 
         calculated = getMultiIndexData(multi, 'date', '2015-01-02', 'category', 'B')
-        expected = pd.Series([2.0], index=pd.MultiIndex(
-            levels=[[u'2015-01-01', u'2015-01-02', u'2015-02-04'], [u'A', u'B', u'C']],
-            labels=[[1], [1]],
-            names=[u'date', u'category']))
+        expected = pd.Series([2.0], index=pd.MultiIndex.from_tuples([('2015-01-02', 'B')],
+            names=['date', 'category']))
         pd.util.testing.assert_series_equal(calculated, expected)
 
         calculated = getMultiIndexData(multi, 'date', ['2015-01-02', '2015-02-04'], 'category', 'C')
-        expected = pd.Series([3.0, 3.0], index=pd.MultiIndex(
-            levels=[[u'2015-01-01', u'2015-01-02', u'2015-02-04'], [u'A', u'B', u'C']],
-            labels=[[1, 2], [2, 2]],
-            names=[u'date', u'category']))
+        expected = pd.Series([3.0, 3.0], index=pd.MultiIndex.from_product(
+            [['2015-01-02', '2015-02-04'], ['C']],
+            names=['date', 'category']))
         pd.util.testing.assert_series_equal(calculated, expected)
 
         calculated = getMultiIndexData(multi, 'date', ['2015-01-02', '2015-02-04'], 'category', ['B', 'C'])
-        expected = pd.Series([2.0, 3.0, 3.0], index=pd.MultiIndex(
-            levels=[[u'2015-01-01', u'2015-01-02', u'2015-02-04'], [u'A', u'B', u'C']],
-            labels=[[1, 1, 2], [1, 2, 2]],
-            names=[u'date', u'category']))
+        expected = pd.Series([2.0, 3.0, 3.0], index=pd.MultiIndex.from_arrays(
+            [['2015-01-02', '2015-01-02', '2015-02-04'], ['B', 'C', 'C']],
+            names=['date', 'category']))
         pd.util.testing.assert_series_equal(calculated, expected)
